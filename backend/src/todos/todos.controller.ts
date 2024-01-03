204e17948619c607f79088b59a7f9472fec98376
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2024-01-03 12:29:11 -0700

    feat: initial todo get/update logic

--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TodosService } from './todos.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
@@ -19,16 +27,16 @@ export class TodosController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.todosService.findOne(+id);
+    return this.todosService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todosService.update(+id, updateTodoDto);
+    return this.todosService.update(id, updateTodoDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.todosService.remove(+id);
+    return this.todosService.remove(id);
   }
 }
--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodosService {
+  todos: Todo[] = [];
+
   create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
+    const todo = new Todo();
+    todo.text = createTodoDto.text;
+    todo.completed = false;
+    this.todos.push(todo);
+    return todo;
   }
 
   findAll() {
     return `This action returns all todos`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  findOne(id: string) {
+    return this.todos.find((todo) => todo.id === id);
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+  update(id: string, updateTodoDto: UpdateTodoDto) {
+    const todo = this.todos.find((todo) => todo.id === id);
+    Object.assign(todo, updateTodoDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} todo`;
+  remove(id: string) {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Cannot delete: todo not found');
+    }
+    this.todos.splice(index, 1);
   }
 }
